eb2671cd2180ebf4545b956bd4e8aa99eeeefad2
Suraj Suryawanshi <113349232+Suraj454@users.noreply.github.com>
2024-01-09 12:47:32 +0530

    Add files via upload

---
+++ Lamp Website/script.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById("btn");
+var light = document.getElementById("light");
+
+function toggleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
